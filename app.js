536d1839ea499bb6ecc73acd9c620933ab00e6d0
James Snell <Vulpes3.1415@yahoo.com>
2021-12-14 18:45:38 -0700

    Added game functionality and scoreboard usage

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-const userScore = 0;
-const computerScore = 0;
+let userScore = 0;
+let computerScore = 0;
 const userScore_span = document.getElementById("user-score");
 const computerScore_span = document.getElementById("computer-score");
 const scoreBoard_div = document.querySelector(".score-board");
-const result_div = document.querySelector(".result");
-const rock_div = document.getElementById("r");
+const result_p = document.querySelector(".result > p");
+const rock_div = document.getElementById("r"); 
 const paper_div = document.getElementById("p");
 const scissors_div = document.getElementById("s");
 
@@ -14,7 +14,33 @@ function getComputerChoice () {
     const randomNumber = Math.floor(Math.random() * 3);
     return choices[randomNumber];
 }
-console.log(getComputerChoice());
+
+function convertToWord(letter) {
+    if(letter === "r") return "Rock";
+    if(letter === "p") return "Paper";
+    return "Scissors";
+}
+
+function win(userChoice, computerChoice) {
+    userScore++;
+    userScore_span.innerHTML = userScore;
+    computerScore_span.innerHTML = computerScore;
+    const smallUserWord = "User".fontsize(3).sub();
+    const smallCompWord = "Comp".fontsize(3).sub();
+    result_p.innerHTML = `${convertToWord(userChoice)}${smallUserWord} beats ${convertToWord(computerChoice)}${smallCompWord    }. You win!`;
+} 
+
+function lose() {
+    computerScore++;
+    
+}
+
+function draw() {
+    console.log("DRAW");
+}
+
+
+
 
 
 function game(userChoice) {
@@ -23,17 +49,17 @@ function game(userChoice) {
         case "rs":
         case "pr":
         case "sp":
-            console.log("User wins.");
+            win(userChoice, computerChoice);
             break;
         case "rp":
         case "ps":
         case "sr":
-            console.log("User loses.");
+            lose(userChoice, computerChoice);
             break;
         case "rr":
         case "pp":
         case "ss":
-            console.log("Its a draw.");
+            draw(userChoice, computerChoice);
             break;
     }
 
